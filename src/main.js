b9fe72efba3cf36b8d31a8990691256663c34351
Han Bing Feng <hangoglebf@gmail.com>
2014-12-17 22:20:29 +0800

    add version

--- src/main.js
+++ src/main.js
@@ -295,7 +295,7 @@ define(function (require) {
     };
     /* eslint-enable fecs-max-statements */
 
-    main.version = '0.0.2-alpha.15';
+    main.version = '0.0.2-alpha.16';
 
     return main;
 });
